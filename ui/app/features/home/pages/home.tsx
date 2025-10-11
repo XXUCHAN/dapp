a8a8e121f04b4c554252e4a8f7fbca49151f003d
XXUCHAN <uyhj0804@ajou.ac.kr>
2025-10-11 21:15:11 +0900

    postgres 연결

--- ui/app/features/home/pages/home.tsx
+++ ui/app/features/home/pages/home.tsx
@@ -1,3 +1,11 @@
+import { supabase } from "~/postgres/supaclient"
+
+//loader - 예약어 함수, 렌더링 전에 실행 서버에서 실행 , 브라우저 X
+export async function loader(){
+    const {data} = await supabase().from("test").select("*");
+    console.log(data);
+}
+
 export default function Home(){
     return <div>
         Hello World
